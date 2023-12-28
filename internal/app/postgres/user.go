056d35c0a97de1294c5c420233b440491b3d947c
Shivam <shivam@nilenso.com>
2023-12-29 00:10:25 +0530

    Add DB level function to insert User

--- internal/app/postgres/user.go
+++ internal/app/postgres/user.go
@@ -27,3 +27,29 @@ func ReadUser(c *gin.Context, postgresDB *sql.DB, username string) (*dto.User, e
 
 	return &user, nil
 }
+
+func InsertUser(c *gin.Context, postgresDB *sql.DB, data interface{}) error {
+	user := data.(dto.User)
+	tx, err := postgresDB.BeginTx(c, nil)
+	defer tx.Rollback()
+	if err != nil {
+		return errors.New("couldn't initiate a database transaction")
+	}
+
+	insertStatement := `INSERT INTO users(id,username, password, email) VALUES ($1, $2, $3, $4)`
+	result, err := tx.ExecContext(c, insertStatement, user.Id, user.Username, user.Password, user.Email)
+	if err != nil {
+		return err
+	}
+
+	numRowsAffected, err := result.RowsAffected()
+	if numRowsAffected != 1 || err != nil {
+		return err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
